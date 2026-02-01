a43cd378cf60d7d4a1cbeac416e074cacbc39c55
Alex <alexom003@hotmail.fr>
2026-02-01 16:16:05 +0100

    Change docker configuration

---
+++ docker/entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+set -e
+
+echo "Démarrage du container Symfony"
+
+# Installer les dépendances PHP si vendor/ n'existe pas
+echo "Installation des dépendances Composer"
+composer install --no-interaction --prefer-dist
+
+
+# Donner les droits nécessaires (cache, logs, db)
+mkdir -p var/cache var/log
+touch var/app.db
+chown -R www-data:www-data var
+chmod -R 775 var
+
+# Lancer les migrations Doctrine
+if [ -f "bin/console" ]; then
+    echo "Lancement des migrations Doctrine"
+    php bin/console make:migration
+    php bin/console doctrine:migrations:migrate --no-interaction || true
+
+    echo "Insertion des fixtures"
+    php bin/console doctrine:fixtures:load --no-interaction || true
+fi
+
+# Initialisation des clés JWT
+if [ ! -f "config/jwt/private.pem" ]; then
+    echo "Génération des clés JWT"
+    php bin/console lexik:jwt:generate-keypair --overwrite
+fi
+
+# Lancer PHP-FPM
+exec php-fpm

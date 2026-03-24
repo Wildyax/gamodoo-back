77171de0323ea89e10a84f6021801d959554b5c8
Alex <alexom003@hotmail.fr>
2026-03-24 09:35:14 +0100

    Update migrations

--- migrations/Version20260324083102.php
+++ migrations/Version20260324083102.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260126192820 extends AbstractMigration
+final class Version20260324083102 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,9 +20,11 @@ final class Version20260126192820 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE job (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description CLOB NOT NULL)');
-        $this->addSql('CREATE TABLE task (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, description VARCHAR(255) NOT NULL, label VARCHAR(255) NOT NULL, level INTEGER NOT NULL, tags CLOB DEFAULT NULL --(DC2Type:array)
-        , checked BOOLEAN NOT NULL)');
+        $this->addSql('CREATE TABLE experience (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, level INTEGER NOT NULL, exp INTEGER NOT NULL)');
+        $this->addSql('CREATE TABLE job (id INTEGER NOT NULL, name VARCHAR(255) NOT NULL, description CLOB NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE task (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, user_id INTEGER NOT NULL, description VARCHAR(255) NOT NULL, label VARCHAR(255) NOT NULL, tags CLOB DEFAULT NULL --(DC2Type:array)
+        , checked BOOLEAN NOT NULL, difficulty INTEGER NOT NULL, CONSTRAINT FK_527EDB25A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('CREATE INDEX IDX_527EDB25A76ED395 ON task (user_id)');
         $this->addSql('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, job_id INTEGER NOT NULL, email VARCHAR(180) NOT NULL, level INTEGER DEFAULT 1 NOT NULL, exp INTEGER DEFAULT 0 NOT NULL, roles CLOB NOT NULL --(DC2Type:json)
         , password VARCHAR(255) NOT NULL, login VARCHAR(255) NOT NULL, CONSTRAINT FK_8D93D649BE04EA9 FOREIGN KEY (job_id) REFERENCES job (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON user (email)');
@@ -33,6 +35,7 @@ final class Version20260126192820 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE experience');
         $this->addSql('DROP TABLE job');
         $this->addSql('DROP TABLE task');
         $this->addSql('DROP TABLE user');
